3b07acee91dad4d9046a1eed606c42be61601a5b
Davina <davzaman@gmail.com>
2023-03-23 16:02:44 -0700

    add script to run all the cohort combinations for experiments

---
+++ module_code/scripts/experiment_grid.py
@@ -0,0 +1,26 @@
+import subprocess
+
+USE_ROLLING_WINDOW = False
+
+if __name__ == "__main__":
+    train_eval_pairs = [
+        ("ucla_crrt", "ucla_crrt"),
+        # ("cedars_crrt", "cedars_crrt"),
+        # ("ucla_crrt", "ucla_control"),
+        # ("ucla_crrt", "cedars_crrt"),
+        # ("cedars_crrt", "ucla_crrt"),
+        # ("ucla_crrt+cedars_crrt", "ucla_crrt+cedars_crrt"),
+    ]
+    for train_val_cohort, eval_cohort in train_eval_pairs:
+        command = [
+            "python",
+            "module_code/scripts/rolling_window_analysis.py"
+            if USE_ROLLING_WINDOW
+            else "module_code/main.py",
+            "--train_val_cohort",
+            train_val_cohort,
+            "--eval_cohort",
+            eval_cohort,
+        ]
+        subprocess.run(command)
+        # print(command)
