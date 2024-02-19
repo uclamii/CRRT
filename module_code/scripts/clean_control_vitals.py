d9defd840da96da6039571bec9550d9089d0d321
Feng <jeffreyfeng@REDLRADADM14958.ad.medctr.ucla.edu>
2024-02-18 16:25:02 -0800

    remove extraneous vitals from controls once. significantly speeds up runtime as this file is huge

---
+++ module_code/scripts/clean_control_vitals.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+if __name__ == "__main__":
+    df = pd.read_csv("../../Data/Controls_v3/Flowsheet_Vitals.txt")
+
+    print(len(df))
+
+    df = df.drop_duplicates(
+        subset=[
+            "IP_PATIENT_ID",
+            "VITAL_SIGN_TYPE",
+            "VITAL_SIGN_TAKEN_TIME",
+            "VITAL_SIGN_VALUE",
+        ]
+    )
+
+    print(len(df))
+
+    df.to_csv("../../Data/Controls_v3/Flowsheet_Vitals_noduplicates.txt", index=False)
